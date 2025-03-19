328694c8aa6da4b04c8a7f8340bd96be6d0040db
Guillermo Cifre <guillecg13@gmail.com>
2025-03-19 17:22:03 +0100

    Modified

--- app/controllers/protected_controller.py
+++ app/controllers/protected_controller.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+from flask_jwt_extended import jwt_required
+from app.views.protected_view import protected_view
 
 protected_bp = Blueprint('protected', __name__)
 
 @protected_bp.route('/protected', methods=['GET'])
+@jwt_required()
 def protected():
-    return 'This is a protected page!'
+    return protected_view()
