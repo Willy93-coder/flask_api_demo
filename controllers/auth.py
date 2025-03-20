bc9eae6adcb0a11bcec71229f8376b84e4905448
Guillermo Cifre <guillecg13@gmail.com>
2025-03-20 17:00:50 +0100

    Added auth controller

---
+++ controllers/auth.py
@@ -0,0 +1,20 @@
+from flask import Request
+from app.models.user import User
+from app.views.auth_view import AuthView
+
+class AuthController:
+  @staticmethod
+  def register_user(request: Request) -> None | str:
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    message = User.register_user(email, password)
+    return AuthView.register_view(message)
+  
+  @staticmethod
+  def login_user(request: Request) -> User | str:
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    user = User.login_user(email, password)
+    return AuthView.login_view(user)
