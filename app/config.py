44ed08ebeb547b65b398319ce356f351a61ebec5
Guillermo Cifre <75251943+Willy93-coder@users.noreply.github.com>
2025-03-20 15:16:11 +0100

    Revert "Feature/jwt"

--- app/config.py
+++ app/config.py
@@ -18,8 +18,6 @@ class Config:
   SQLALCHEMY_DATABASE_URI = f'{DB}+{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-  JWT_SECRET_KEY = os.environ.get('JWT_SECRET')
-
 class DevelopmentConfig(Config):
   """Development configuration"""
   DEBUG = True
--- app/views/protected_view.py
+++
@@ -1,7 +0,0 @@
-from typing import Literal
-from flask import Response, jsonify
-from app.services.protected_service import protected_service
-
-def protected_view() -> (tuple[Response, Literal[200]]):
-    message = protected_service()
-    return jsonify(message=message), 200
