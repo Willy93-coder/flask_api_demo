87fe657318cc11a0416f7e59c0f6bdbf34b87e4d
Guillermo Cifre <guillecg13@gmail.com>
2025-03-20 16:58:25 +0100

    Added static methods

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
 from datetime import datetime, timezone
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app.db.database import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = "user_account"
@@ -19,3 +21,28 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.email
+    
+    @staticmethod
+    def register_user(email: str, password: str) -> None | str:
+        if User._get_user_by_email(email):
+            return "User already exists"
+        hashed_pw = User._hash_password(password)
+        user = User(email=email, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        return None
+
+    @staticmethod
+    def login_user(email: str, password: str) -> User | None:
+        user = User._get_user_by_email(email)
+        if not user or not check_password_hash(user.password, password):
+            return None
+        return user
+
+    def _hash_password(password: str) -> str:
+        hashed_pw = generate_password_hash(password)
+        return hashed_pw
+
+    def _get_user_by_email(email: str) -> User | None:
+        user = db.session.query(User).filter(User.email == email).first()
+        return user
