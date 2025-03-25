8db9417939d83b15d3006c26decefa1f1be02eb0
Guillermo Cifre <guillecg13@gmail.com>
2025-03-25 09:42:47 +0100

    Modified values of function return

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,10 +1,11 @@
-from flask import Request
+from typing import Literal
+from flask import Request, Response
 from app.models.user import User
 from app.views.auth import AuthView
 
 class AuthController:
   @staticmethod
-  def register_user(request: Request) -> None | str:
+  def register_user(request: Request) -> (tuple[Response, Literal[201]] | tuple[Response, Literal[400]]):
     data = request.get_json()
     email = data.get('email')
     password = data.get('password')
@@ -12,7 +13,7 @@ class AuthController:
     return AuthView.register_view(message)
   
   @staticmethod
-  def login_user(request: Request) -> User | str:
+  def login_user(request: Request) -> (tuple[Response, Literal[200]] | tuple[Response, Literal[401]]):
     data = request.get_json()
     email = data.get('email')
     password = data.get('password')
